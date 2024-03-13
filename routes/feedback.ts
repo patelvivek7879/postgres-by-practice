73724a5877ed69a69da2c2e32a2a3028e35c9347
patelvivek7879 <patelvivek7879@gmail.com>
2024-03-14 00:34:43 +0530

    feedbacks get api to get feedbacks, added basic pagination and search

--- routes/feedback.ts
+++ routes/feedback.ts
@@ -6,13 +6,80 @@ import express, {
   NextFunction,
 } from "express";
 import { mustBeAuthenticated } from "../middleware/authetication";
-import fs from "fs";
-import { logger } from "../utils/logger";
 import { User } from "../types/User";
 import { prisma } from "../repository/User";
 import { rateLimiter } from "../middleware/rateLimiter";
 
 const router = express.Router();
+
+router.get(
+  "/api/v1/feedback",
+  // mustBeAuthenticated,
+  async (req: Request, res: Response) => {
+    const { offset, size, search } = req.query;
+
+    try {
+      const getCountWhereClause: any = () => {
+        if (search && search.length && search !== "") {
+          return {
+            where: {
+              message: {
+                search: search,
+              },
+            },
+          };
+        } else {
+          return {};
+        }
+      };
+
+      const getSearchTermWhereClause: any = () =>{
+        if(search && search !== "") {
+          return {
+            where: {
+              message: {
+                search: search as unknown as string,
+              },
+            }
+          }
+        }else{
+          return {}
+        }
+      }
+
+      const feedbackData: any = await prisma.$transaction([
+        prisma.feedbacks.count(getCountWhereClause()),
+        prisma.feedbacks.findMany({
+          skip: Number(offset),
+          take: Number(size),
+          where: {
+            message: {
+              search: search as unknown as string,
+            },
+          }
+        }),
+      ]);
+
+      const data = feedbackData[1].map((d: any) => ({
+        ...d,
+        createdAt: `${d.createdAt}`,
+      }));
+
+      res.status(200).json({
+        status: 200,
+        message: "Feedback data fetched successfully",
+        data: data,
+        total: feedbackData[0],
+      });
+    } catch (error) {
+      console.error(" Error while inserting feedback ", error);
+      res.status(500).json({
+        status: 500,
+        message: "Something went wrong, Fail to fetch feedback data",
+      });
+    }
+  }
+);
 
 router.post(
   "/api/v1/feedback",
